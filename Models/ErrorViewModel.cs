6dab8565a642b1bd2041aa79b3248c5086619a1e
Victor Moraes <victor.moraes@outlook.com.br>
2026-09-24 20:08:38 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HouseholdTasks.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
